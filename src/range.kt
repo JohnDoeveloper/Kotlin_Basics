bd298bff8ba96cae8d590bda5bcc27563817dab5
MacPro <macpro@192.168.1.8>
2019-04-24 17:48:14 +0200

    Initial commit

---
+++ src/range.kt
@@ -0,0 +1,56 @@
+fun main() {
+
+    // Numeric data type
+
+    val byteMin: Byte = Byte.MIN_VALUE
+    val byteMax: Byte = Byte.MAX_VALUE
+
+    val unsignedByteMin: UByte = UByte.MIN_VALUE
+    val unsignedByteMax: UByte = UByte.MAX_VALUE
+
+
+        println("\nByte: \n\n\t-min value: $byteMin\n\t-max value: $byteMax\n\t -unsingned min: $unsignedByteMin\n\t -unsigned max: $unsignedByteMax")
+
+    val shortMin: Short = Short.MIN_VALUE
+    val shortMax: Short = Short.MAX_VALUE
+
+    val unsignedShortMin: UShort = UShort.MIN_VALUE
+    val unsignedShortMax: UShort = UShort.MAX_VALUE
+
+        println("\nShort: \n\n\t-min value: $shortMin\n\t-max value: $shortMax\n\t -unsigned min $unsignedShortMin\n\t -unsigned mnax: $unsignedShortMax")
+
+    val integerMin: Int = Int.MIN_VALUE
+    val integerMax: Int = Int.MAX_VALUE
+
+    val unsignedIntMin: UInt = UInt.MIN_VALUE
+    val unsignedIntMax: UInt = UInt.MAX_VALUE
+
+
+    println("\nInteger: \n\n\t-min value: $integerMin\n\t-max value: $integerMax\n\t -unsigned min: $unsignedIntMin\n\t -unsingned max: $unsignedIntMax")
+
+    val floatMin: Float = Float.MIN_VALUE
+    val floatMax: Float = Float.MAX_VALUE
+
+
+    println("\nFloat: \n\n\t-min value: $floatMin\n\t-max value: $floatMax")
+
+    val doubleMin: Double = Double.MIN_VALUE
+    val doubleMax: Double = Double.MAX_VALUE
+
+
+    println("\nDouble: \n\n\t-min value: $doubleMin\n\t-max value: $doubleMax")
+
+    val longMin: Long = Long.MIN_VALUE
+    val longMax: Long = Long.MAX_VALUE
+
+
+    val unsignedLongMin: ULong = ULong.MIN_VALUE
+    val unsignedLongMax: ULong = ULong.MAX_VALUE
+
+
+    println("\nLong: \n\n\t-min value: $longMin\n\t-max value: $longMax\n\t -unsigned min: $unsignedLongMin\n\t -unsigned max: $unsignedLongMax")
+
+
+
+}
+
